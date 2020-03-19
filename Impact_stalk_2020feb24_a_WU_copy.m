d41b778994f1724aa117c95487854da6440276e0
lichen <lichen@hydrolab.com>
2020-03-18 20:36:02 -0400

    add level to maxHeight file

--- Impact_stalk_2020feb24_a_WU_copy.m
+++ Impact_stalk_2020feb24_a_WU_copy.m
@@ -356,7 +356,7 @@ for movie_itr = 6: 8
 
 
             if heightYY< maxHeightYY
-                maxHeightYY = heightYY;
+                maxHeightYY = heightYY
             end
                 
 % saved max height
@@ -417,7 +417,7 @@ for movie_itr = 6: 8
     
     fid = fopen(maxHeightfile,'a');
     fprintf(fid,'%s',[img_dir.folder]);
-    fprintf(fid, '\t %d\n',[maxHeightYY]); %relative to flat surface
+    fprintf(fid, '\t %d  \t %d\n',[maxHeightYY;level]); %relative to flat surface
     fclose(fid);
     cd ..
 
